bf0c55c6afb781d329b4fae9ecab5a83608a2a7b
AltafMohdShaikh <altafmohdshaikh@gmail.com~>
2024-11-11 03:51:42 +0530

    final changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -26,11 +26,11 @@ setCount(count-1)
   }
   return (
     <div className='flex mt-5 p-5 justify-center flex-wrap'>
-      <div className='font-Bungee text-7xl p-28 border-8 border-blue-600 rounded-full mr-5'>
+      <div className='font-Bungee text-7xl p-28 md:w-60 '>
         <p> {count}</p>
        
       </div>
-      <div className='items-center flex justify-center flex-col ml-5'>
+      <div className='items-center flex justify-center flex-col md:ml-52'>
       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-52 mt-10 ml-5 mr-5 px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         onClick={increment} >Increment</button>
 
